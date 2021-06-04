33bea530cf0006c4f31ecceb6de1643d39798f8c
Danny White <hi@dannywhite.site>
2021-06-04 13:40:53 +1000

    feat: frameCount stuff in sprite class

--- sketch.js
+++ sketch.js
@@ -1,15 +1,16 @@
 let spritesheet;
 
-let animationIn = [];
-let animationMiddle = [];
-let animationOut = [];
+// let animationIn = [];
+// let animationMiddle = [];
+// let animationOut = [];
+let ballSprites = ['img/ball/spritesheet-in.jpg', 'img/ball/spritesheet-middle.jpg', 'img/ball/spritesheet-out.jpg']
 
 let playState = "off"
 
 function preload() {
-  spritesheetIn = loadImage('img/spritesheet-in.jpg');
-  spritesheetMiddle = loadImage('img/spritesheet-middle.jpg');
-  spritesheetOut = loadImage('img/spritesheet-out.jpg');
+  ballSpriteSheetIn = loadImage('img/ball/spritesheet-in.jpg');
+  ballSpriteSheetMiddle = loadImage('img/ball/spritesheet-middle.jpg');
+  ballSpriteSheetOut = loadImage('img/ball/spritesheet-out.jpg');
 }
 
 
@@ -17,28 +18,28 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   background(255);
 
-  //  Automatically count the number of frames by dividing the sprightsheet height by width
-  // (assumes frames are square so the height of each square is the same as the entire width)
-  let frameCountIn = spritesheetIn.height / spritesheetIn.width;
-  let frameCountMiddle = spritesheetMiddle.height / spritesheetMiddle.width;
-  let frameCountOut = spritesheetOut.height / spritesheetOut.width;
+  //   //  Automatically count the number of frames by dividing the sprightsheet height by width
+  // // (assumes frames are square so the height of each square is the same as the entire width)
+  // let frameCountIn = spritesheetIn.height / spritesheetIn.width;
+  // let frameCountMiddle = spritesheetMiddle.height / spritesheetMiddle.width;
+  // let frameCountOut = spritesheetOut.height / spritesheetOut.width;
 
-  // Create img arrays for each of the three scenes
-  for (let i = 0; i < frameCountIn; i++) {
-    let img = spritesheetIn.get(0, i * spritesheetIn.width, spritesheetIn.width, spritesheetIn.width);
-    animationIn.push(img);
-  }
-  for (let i = 0; i < frameCountMiddle; i++) {
-    let img = spritesheetMiddle.get(0, i * spritesheetMiddle.width, spritesheetMiddle.width, spritesheetMiddle.width);
-    animationMiddle.push(img);
-  }
-  for (let i = 0; i < frameCountOut; i++) {
-    let img = spritesheetOut.get(0, i * spritesheetOut.width, spritesheetOut.width, spritesheetOut.width);
-    animationOut.push(img);
-  }
+  // // Create img arrays for each of the three scenes
+  // for (let i = 0; i < frameCountIn; i++) {
+  //   let img = spritesheetIn.get(0, i * spritesheetIn.width, spritesheetIn.width, spritesheetIn.width);
+  //   animationIn.push(img);
+  // }
+  // for (let i = 0; i < frameCountMiddle; i++) {
+  //   let img = spritesheetMiddle.get(0, i * spritesheetMiddle.width, spritesheetMiddle.width, spritesheetMiddle.width);
+  //   animationMiddle.push(img);
+  // }
+  // for (let i = 0; i < frameCountOut; i++) {
+  //   let img = spritesheetOut.get(0, i * spritesheetOut.width, spritesheetOut.width, spritesheetOut.width);
+  //   animationOut.push(img);
+  // }
 
   // Create sprite based on these three 
-  ball = new Sprite(animationIn, animationMiddle, animationOut, 0, 0, 300, 0.1);
+  ball = new Sprite(ballSpriteSheetIn, ballSpriteSheetMiddle, ballSpriteSheetOut, 0, 0, 300, 0.1);
   // spike = new Sprite(animationIn, animationMiddle, animationOut, 0, 0, 300, 0.1);
 }
 
--- sprite.js
+++ sprite.js
@@ -1,12 +1,19 @@
 // This class takes three animations and figures out the frames
 class Sprite {
-  constructor(animationIn, animationMiddle, animationOut, x, y, width, speed) {
+  constructor(spriteSheetIn, spriteSheetMiddle, spriteSheetOut, x, y, width, speed) {
+    //  Automatically count the number of frames by dividing the sprightsheet height by width
+    // (assumes frames are square so the height of each square is the same as the entire width)
+    this.frameCountIn = spriteSheetIn.height / spriteSheetIn.width;
+    this.frameCountMiddle = spriteSheetMiddle.height / spriteSheetMiddle.width;
+    this.frameCountOut = spriteSheetOut.height / spriteSheetOut.width;
+
+
     this.x = x;
     this.y = y;
     this.width = width;
-    this.animationIn = animationIn;
-    this.animationMiddle = animationMiddle;
-    this.animationOut = animationOut;
+    this.animationIn = [];
+    this.animationMiddle = [];
+    this.animationOut = [];
     this.speed = speed;
     this.indexIn = 0;
     this.indexMiddle = 0;
@@ -17,46 +24,60 @@ class Sprite {
     this.showExitScene = false;
     this.enterPlayProgress = 0;
     this.exitPlayProgress = 0;
+
+    // Create img arrays for each of the three scenes
+    for (let i = 0; i < this.frameCountIn; i++) {
+      let img = spriteSheetIn.get(0, i * spriteSheetIn.width, spriteSheetIn.width, spriteSheetIn.width);
+      this.animationIn.push(img);
+    }
+    for (let i = 0; i < this.frameCountMiddle; i++) {
+      let img = spriteSheetMiddle.get(0, i * spriteSheetMiddle.width, spriteSheetMiddle.width, spriteSheetMiddle.width);
+      this.animationMiddle.push(img);
+    }
+    for (let i = 0; i < this.frameCountOut; i++) {
+      let img = spriteSheetOut.get(0, i * spriteSheetOut.width, spriteSheetOut.width, spriteSheetOut.width);
+      this.animationOut.push(img);
+    }
   }
 
   // In
   enterStage() {
     if (this.showEnterStage) {
-    // Rewind the exit animation from last time
-    this.enterPlayProgress = 0;
-    // Disable the hold stage for showing, for now
-    this.showHoldScene = false;
-    // Set the animation index
-    let indexInFloored = floor(this.indexIn) % this.animationIn.length;
-    // Create the image
-    image(this.animationIn[indexInFloored], this.x, this.y, this.width, this.width);
+      // Rewind the exit animation from last time
+      this.enterPlayProgress = 0;
+      // Disable the hold stage for showing, for now
+      this.showHoldScene = false;
+      // Set the animation index
+      let indexInFloored = floor(this.indexIn) % this.animationIn.length;
+      // Create the image
+      image(this.animationIn[indexInFloored], this.x, this.y, this.width, this.width);
 
-    // Enable the exit for showing
-    // TODO: Make this enter scene completely play out before triggering exit
-    this.showExitScene = true;
+      // Enable the exit for showing
+      // TODO: Make this enter scene completely play out before triggering exit
+      this.showExitScene = true;
 
-    // Check each draw() for if this has finished all frames
-    if (indexInFloored < this.animationIn.length - 1 && this.enterPlayProgress !== 1) {
-      // If not finished, progress another frame
-      this.indexIn += this.speed;
-    } else {
-      // console.log("Finished!")
-      // If finished
-      // Finish the exit animation
-      this.enterPlayProgress = 1;
-      // console.log ("finished")
-      // this.enterPlayProgress = 1;
-      // Don't let this function continue again
-      this.showEnterStage = false;
-      // // Rewind the index for next time
-      this.indexIn = 0;
-      // Enable the hold stage for showing
-      this.showHoldScene = true;
-      
-      // return this.showHoldScene;
-      // this.holdStage()
+      // Check each draw() for if this has finished all frames
+      if (indexInFloored < this.animationIn.length - 1 && this.enterPlayProgress !== 1) {
+        // If not finished, progress another frame
+        this.indexIn += this.speed;
+      } else {
+        // console.log("Finished!")
+        // If finished
+        // Finish the exit animation
+        this.enterPlayProgress = 1;
+        // console.log ("finished")
+        // this.enterPlayProgress = 1;
+        // Don't let this function continue again
+        this.showEnterStage = false;
+        // // Rewind the index for next time
+        this.indexIn = 0;
+        // Enable the hold stage for showing
+        this.showHoldScene = true;
+
+        // return this.showHoldScene;
+        // this.holdStage()
+      }
     }
-  }
   }
 
   // Middle
